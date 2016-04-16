0a07d3e70c4088b15b0898e954c2905369b244c0
Arran Schlosberg <arran@oonix.com.au>
2016-04-16 22:38:37 +0100

    Added license (MIT) + setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="BNOL",
+    version="0.1",
+    author="Arran Schlosberg",
+    author_email="arran@oonix.com.au",
+    license="MIT",
+    install_requires=[
+        "numpy",
+        "scipy",
+    ]
+)
