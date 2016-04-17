4df26cd372a870bc118ec1f248a707103061e1cc
Arran Schlosberg <arran@oonix.com.au>
2016-04-17 11:41:33 +0100

    Use at least Sphinx 1.3 so that sphinxcontrib.napoleon is available in Read The Docs for parsing Google docstrings.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,17 +15,16 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath('../..')) # get the path to BNoL development directory
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+needs_sphinx = '1.3' # 1.3+ for automatic inclusion of sphinxcontrib-napoleon
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -36,6 +35,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
