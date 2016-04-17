424536da40998e575dd435e7a4e862bf44190bc4
Arran Schlosberg <arran@oonix.com.au>
2016-04-17 12:19:50 +0100

    Reduce required Sphinx version to 1.3 for compatibility with Read The Docs. Can not go lower if using Napoleon extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '1.4' # 1.3+ for automatic inclusion of sphinxcontrib-napoleon
+needs_sphinx = '1.3' # 1.3+ for automatic inclusion of sphinxcontrib-napoleon as sphix.ext.napoleon
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
