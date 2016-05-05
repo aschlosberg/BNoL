92b1b80e5f9fbe9b11eabccb59afb29bb0b99162
Arran Schlosberg <arran@oonix.com.au>
2016-05-05 15:44:18 +0100

    Modularisation of cosine distance to utilise scipy.spacial.distance.pdist + integration with Jensen-Shannon divergence as calculated by bnol.information.Divergence().JS()

--- bnol/distance.py
+++ bnol/distance.py
@@ -1,6 +1,19 @@
 import numpy as np
 from scipy.spatial import distance
 from math import sqrt
+from . import information
+
+def Generic(samples, fn):
+    """Convenience wrapper for scipy.spacial.distance.squareform(scipy.spatial.distance.pdist(samples, fn))
+
+    Args:
+        samples (numpy.ndarray): shape (n,p); where n is the number of samples
+        fn (str or function(sample, sample) => float): distance measure that returns a float for given samples
+
+    Returns:
+        numpy.ndarray: shape (n,p); symmetrical; zero-diagonals; pair-wise distance between samples
+    """
+    return distance.squareform(distance.pdist(samples, metric=fn))
 
 def Cosine(samples):
     """Calculate a pair-wise distance matrix for a set of samples based on the cosine between their values.
@@ -11,10 +24,16 @@ def Cosine(samples):
     Returns:
         numpy.ndarray: shape (n,p); symmetrical; zero-diagonals; pair-wise distance between samples
     """
-    nSamples = samples.shape[0]
-    similarity = np.zeros((nSamples, nSamples), dtype='float')
-    for i in range(nSamples):
-        for j in range(i):
-            similarity[i,j] = distance.cosine(samples[i,:], samples[j,:])
-            similarity[j,i] = similarity[i,j]
-    return similarity
+    return Generic(samples, 'cosine')
+
+def JensenShannon(samples):
+    """Calculate a pair-wise distance matrix for a set of samples based on Jensen-Shannon divergence between their values.
+
+    Args:
+        samples (numpy.ndarray): shape (n,p); where n is the number of samples
+
+    Returns:
+        numpy.ndarray: shape (n,p); symmetrical; zero-diagonals; pair-wise distance between samples
+    """
+    distFn = lambda P, Q: information.Divergence(P, Q).JS()
+    return Generic(samples, fn=distFn)
--- tests/distance.py
+++ tests/distance.py
@@ -11,11 +11,12 @@ class DistanceTester(NumpyfiedTestCase):
 
     def setUp(self):
         self.samples = np.asarray([
-            [0,1,2,3,4],
-            [0,1,2,3,4],
-            [1,0,0,0,0],
+            [1,2,3],
+            [1,2,3], # identical to first
+            [1,0,0],
+            [0,1,0], # orthogonal to previous
         ])
-        self.nSamples = 3
+        self.nSamples = 4
 
     def _common_distance_matrix_tests(self, fn):
         distances = getattr(distance, fn)(self.samples)
@@ -23,12 +24,16 @@ class DistanceTester(NumpyfiedTestCase):
         self.assertEqual(distances.shape[0], distances.shape[1], "Distance matrices should be square (%s)" % fn)
         self.assertEqual(distances.shape[0], self.nSamples, "Distance matrices should be pair-wise, and thus have same dimension length as number of samples (%s)" % fn)
         self.assertAllTrue(np.diag(distances)==0, "Sample distance from itself must be zero; non-zero value on diagonal (%s)" % fn)
-        self.assertAllTrue(np.asarray([distances[0,1], distances[1,0]])==0, "Identically-valued samples should have zero distance (%s)" % fn)
+        self.assertEqual(distances[0,1], distances[1,0], "Identically-valued samples should have zero distance (%s)" % fn)
         self.assertAllTrue(distances==distances.T, "Distance matrices should be symmetrical; transpose is not equal to original (%s)" % fn)
-        self.assertEqual(distances[0,2], 1, "Distance between orthogonal vectors should be one (%s)" % fn)
+        return distances
 
     def test_cosine_distance(self):
-        self._common_distance_matrix_tests("Cosine")
+        distances = self._common_distance_matrix_tests("Cosine")
+        self.assertEqual(distances[2,3], 1, "Cosine distance between orthogonal vectors should be one")
+
+    def test_jensen_shannon_divergence_distance(self):
+        distances = self._common_distance_matrix_tests("JensenShannon")
 
 if __name__=='__main__':
     unittest.main()
